1d7ae486739ab50bf9ac7de283fd66ee6e735f8f
Maria del Mar <maria.arbelaezs@udea.edu.co>
2021-12-08 23:39:38 -0500

    ejs 1 3 5(último sin completar)

---
+++ lab1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+/* //Ejercicio 1
+int main()
+{
+    short int vabA;
+    short int vabB;
+    cout << "Ingrese un numero A: " <<endl;
+    cin >> vabA;
+    cout << "Ingrese un numero B: " <<endl;
+    cin >> vabB;
+    cout<<"El residuo de A/B es: "<<vabA % vabB<<endl;
+    return 0;
+}
+*/
+/*//Ejercicio 3
+int main()
+{
+    short int vabA;
+    short int vabB;
+    cout << "Ingrese un numero A: " <<endl;
+    cin >> vabA;
+    cout << "Ingrese un numero B: " <<endl;
+    cin >> vabB;
+    if (vabA>vabB){
+        cout<<"El mayor es: "<<vabA<<endl;
+    }
+    else {
+        cout<<"El mayor es: "<<vabB<<endl;
+    }
+
+    return 0;
+}
+*/
+//Ejercicio 5
+int main()
+{
+    short int vabA;
+    short int vabB;
+    float divi;
+    cout << "Ingrese un numero A: " <<endl;
+    cin >> vabA;
+    cout << "Ingrese un numero B: " <<endl;
+    cin >> vabB;
+    divi= float vabB/2
+    if((vabA%vabB)>=divi){
+        cout<<vabA<<"/"<<vabB<<"="<<(vabA/vabB)+1<<endl;
+    }
+    else{
+        cout<<vabA<<"/"<<vabB<<"="<<(vabA/vabB)<<endl;
+    }
+
+    return 0;
+}
